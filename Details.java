8ddde50a3ec6b826509d14d7a7e681eeef5d58d1
Tomba-Bobmanuel <tomba.bobmanuel@gmail.com>
2025-07-30 17:58:01 +0100

    My details

---
+++ Details.java
@@ -0,0 +1,7 @@
+public class Details {
+	public static void main(String[] args){
+		System.out.println("My name is Tomba Bobmanuel Joy Ibifiri. ");
+		System.out.println("I am 16yrs old.");
+		System.out.println("I go to Pan Atlantic University to school and honestly i love it there I can't lie.");
+	}
+}
