8d246f83a1d40bcff024c0729e55d32136cfbf46
Tomba-Bobmanuel <tomba.bobmanuel@gmail.com>
2025-07-31 15:19:07 +0100

    Primitive Datatype

---
+++ PrimitiveDatatype.java
@@ -0,0 +1,29 @@
+public class PrimitiveDatatype{
+	public static void main(String[] args){
+		//byte,short,int,long,float,double,char,boolean
+		byte age = 31;
+		System.out.println("age="+age);
+		
+		short quantity = 20000;
+		System.out.println("The quantity of goods I ordered is "+quantity);
+		
+		int population = 2000000000;
+		System.out.println("Nigeria's population is "+population);
+		
+		long worldspopulation = 7889898988989L;
+		System.out.println("The worlds population is "+worldspopulation);
+		
+		float weight = 76.456F;
+		System.out.println("My weight is "+weight);
+		
+		double accountBalance = 58980.76898887889;
+		System.out.println("My account balance is" +"$" +accountBalance);
+		
+		char symbol = '?';
+		System.out.println(" What is your name"+symbol);
+		
+		boolean isjavafun = true;
+		System.out.println("Is Java Fun " +isjavafun);
+		
+	}
+}
